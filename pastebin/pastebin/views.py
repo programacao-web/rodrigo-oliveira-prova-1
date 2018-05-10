a831c9b25e44ff9f3366859fbf049f64425e16dd
rodrigocam <rodrigo.redcode@gmail.com>
2018-05-10 18:33:13 -0300

    Mudando layout

--- pastebin/pastebin/views.py
+++ pastebin/pastebin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-
+from django.db.models import Q
 from pygments import highlight
 from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
@@ -29,7 +29,7 @@ def language_list(request, language):
     elif language == 'js':
         pastes = Paste.objects.filter(language='javascript')
     else:
-        pastes = Paste.objects.all()
+        pastes = Paste.objects.exclude(Q(language='python') | Q(language='javascript'))
 
     ctx = {'pastes': pastes, 'language': language}
     return render(request, 'pastebin/paste-language.jinja2', ctx)
